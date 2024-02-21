588ad45cfc1f2416a34f79c14e994fb312ae27f3
Laura Sirigu <laurasirigu43@gmail.com>
2024-02-21 15:58:24 +0100

    feat || aggiunto la singola pagina per il customPokemon(manca solo il form per patchare il pokemon) + completato e rifinito page myteam (non funziona perfattamente)

--- src/Pages/MyTeam.jsx
+++ src/Pages/MyTeam.jsx
@@ -1,35 +1,71 @@
 import axios from "axios";
 import { useUser } from "../context/UserContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { axiosHeaders } from "../../lib/utilities";
+import { Link } from "react-router-dom";
+const { VITE_API_URL } = import.meta.env;
 
 export default () => {
 
-    //creare uno state team([]);
-    //creare useEffect con chiamata axios.get a user/:id
-    //salvare la response (user) nello state team(user.team)
-    //(response.data.team)
-    //map di team con creazione piccole card con link a pagina del customPokemon singolo ed editabile.
-    //?.map
-
     const { user, token } = useUser();
-    const [error, setError] = useState();
+    const [error, setError] = useState(false);
+    const [team, setTeam] = useState([]);
+    const [customPokemon, setCustomPokemon] = useState(null);
+
+    useEffect(() => {
+        const fetchTeam = async () => {
+            try {
+                if (user) {
+                    const response = axios.get(`${VITE_API_URL}/user/${user._id}`, axiosHeaders(token));
+                    setTeam(user.team);
+                }
+            } catch (error) {
+                console.error(error);
+                setError(true);
+            }
+        };
+
+        const fetchCustomPokemon = async () => {
+            try {
+                const response = await axios.get(`${VITE_API_URL}/custompokemons`, axiosHeaders(token))
+                setCustomPokemon(response.data);
+            } catch (error) {
+                console.error(error);
+                setError(true);
+            }
+        };
+        fetchTeam();
+        fetchCustomPokemon();
+    }, [user, token]);
+
+
 
     return (
         <div className="my-team page">
             <h1>My Team</h1>
-            <ul>
-                {console.log(user)}
-                {user.team.map((cpId) => {
-                    let cPokemon;
-                    const fetchPokemon = await axios.get(`${VITE_API_URL}/custompokemons`, axiosHeaders(token))
-                    try {
-                        cPokemon = res.data;
+            <ul className="pokemon-list">
+                {team.map((cp) => {
+                    return (
+                        <Link to={`/custompokemons/${cp._id}`} key={cp._id}>
+                            <li className="pokemon-card">
+                                {customPokemon ? (
+                                    <>
+                                        <figure>
+                                            <img src={cp.image} alt={`Picture of ${cp.name}`} />
+                                        </figure>
+                                        <p><strong>Name:</strong> {cp.name}</p>
+                                        <p><strong>ID:</strong> {cp.id}</p>
+                                    </>
+                                ) : (
+                                    <div className="loader">
+                                        <img src="/pokemon-loader.gif" alt="loader gif" />
+                                    </div>
+                                )}
+                            </li>
+                        </Link>
 
-                    } catch (error) {
-                        console.error(error);
-                        setError(true);
-                        return <li key={ }>{pokemon.name}</li>
-                    })}
+                    );
+                })}
             </ul>
         </div>
     )
---
+++ src/components/CustomPokemon.jsx
@@ -0,0 +1,96 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { axiosHeaders } from "../../lib/utilities";
+import { useUser } from "../context/UserContext";
+import { useParams } from "react-router-dom";
+const { VITE_API_URL } = import.meta.env;
+
+export default () => {
+
+    const { token } = useUser();
+    const [error, setError] = useState(false);
+    const [customPokemon, setCustomPokemon] = useState(null);
+    const [confirmDelete, setConfirmDelete] = useState(false);
+
+    const { id } = useParams();
+
+
+    useEffect(() => {
+        const fetchCustomPokemon = async () => {
+            try {
+                const response = await axios.get(`${VITE_API_URL}/custompokemons/${id}`, axiosHeaders(token));
+                setCustomPokemon(response.data);
+            } catch (error) {
+                console.error(error);
+                setError(true)
+            }
+        };
+        fetchCustomPokemon();
+    }, [id]);
+
+
+    const handleReleasePokemon = async () => {
+        try {
+            await axios.delete(`${VITE_API_URL}/custompokemons/${id}`, axiosHeaders(token));
+            setCustomPokemon(null);
+        } catch (error) {
+            console.error('There was an error while releasing your pokemon:', error);
+        }
+    };
+
+    const handleConfrimRelease = () => {
+        setConfirmDelete(true);
+    };
+
+    const handleCancelRelease = () => {
+        setConfirmDelete(false)
+    };
+
+    const handleConfirm = () => {
+        handleReleasePokemon();
+        setConfirmDelete(false);
+    }
+
+    if (!customPokemon) {
+        return (
+            <div>
+                <p>This pokemon was released.</p>
+            </div>
+        )
+    };
+
+    return (
+        <div className="customPokemon page">
+            <h1>ID {customPokemon.id} - {customPokemon.name}</h1>
+            <figure>
+                <img src={customPokemon.image} alt={`Picture of ${customPokemon.name} `} />
+            </figure>
+            <p><strong>Nickname:</strong>{customPokemon.nickname}</p>
+            {customPokemon.type && (
+                <div>
+                    <p><strong>Type:</strong> {Array.isArray(customPokemon.type) ? customPokemon.type.join(', ') : customPokemon.type}</p>
+                </div>
+            )}
+            <p><strong>Level:</strong> {customPokemon.level}</p>
+            {customPokemon.attacks && (
+                <div>
+                    <p><strong>Attacks:</strong> {Array.isArray(customPokemon.attacks) ? customPokemon.attacks.join(', ') : customPokemon.attacks}</p>
+                </div>
+            )}
+            <p><strong>Description:</strong>{customPokemon.description}</p>
+            {!confirmDelete ? (
+                <button onClick={handleConfrimRelease}>Release pokemon</button>
+
+            ) : (
+                <div className="confirm-delete-popup">
+                    <p>Are you sure that you want to release this pokemon?</p>
+                    <div className="delete-btn">
+                        <button onClick={handleConfirm}>Bye bye!</button>
+                        <button onClick={handleCancelRelease}>Stay with me.</button>
+                    </div>
+
+                </div>
+            )}
+        </div>
+    )
+};
